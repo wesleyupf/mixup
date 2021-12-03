cddf4f8571376e96fd1bafba295cc764d5c56d71
Agência UPFlex <agencia.upflex@gmail.com>
2021-12-03 00:07:05 -0300

    fix: corrigindo sanitize

--- src/Utils/Fields/Sanitize.php
+++ src/Utils/Fields/Sanitize.php
@@ -10,14 +10,14 @@ abstract class Sanitize extends Base
     use Response;
 
     /**
-     * @return array
+     * @return string[][]
      */
     abstract protected static function setFields(): array;
 
     protected static function getFields(): array
     {
-        $class = get_class((object)self::$instance);
-        return call_user_func([$class, 'getFields']);
+        $class = get_called_class();
+        return call_user_func([$class, 'setFields']);
     }
 
     /**
@@ -30,12 +30,16 @@ abstract class Sanitize extends Base
 
         foreach ($fields as $field) {
             if (isset($field[0])) {
-                $value = $_POST[$field[0]] ?? null;
-                $data[$field[0]] = self::sanitizeTypeInput($value, $field);
+                $method = in_array('GET', is_array($field) ? $field : []);
+                $method = $method || in_array('GET', $fields);
+
+                $value = $method ? $_GET[$field[0]] ?? '' : $_POST[$field[0]] ?? '';
+
+                $data[$field[0]] = $value ? self::sanitizeTypeInput($value, $field) : null;
             }
         }
 
-        return $data;
+        return array_filter($data);
     }
 
     protected static function sanitizeTypeInput(string $value, array $args = []): string
