b992c86c059102af9afe7a25f2e6bc8148fc3db0
Agência UPFlex <agencia.upflex@gmail.com>
2021-12-04 20:25:44 -0300

    feat: melhorando validação dos campos

--- src/Utils/Fields/Sanitize.php
+++ src/Utils/Fields/Sanitize.php
@@ -9,10 +9,8 @@ trait Sanitize
     use Response;
 
     /**
-     * @return string[][]
+     * @return array
      */
-    abstract protected static function setFields(): array;
-
     protected static function getFields(): array
     {
         $class = get_called_class();
@@ -20,39 +18,51 @@ trait Sanitize
     }
 
     /**
+     * @return string[][]
+     */
+    abstract protected static function setFields(): array;
+
+    /**
+     * @param string $request
      * @return array
      */
-    protected static function sanitizeFields(): array
+    protected static function sanitizeFields(string $request = 'post'): array
     {
+        $allow_methods = ['get' => $_GET, 'post' => $_POST];
+
         $fields = self::getFields();
         $data = [];
 
-        foreach ($fields as $field) {
-            if (isset($field[0])) {
-                $method = in_array('GET', is_array($field) ? $field : []);
-                $method = $method || in_array('GET', $fields);
+        foreach ($fields as $key => $expected) {
+            $method = array_key_exists($request, $allow_methods)
+                ? $allow_methods[$request]
+                : $allow_methods['post'];
 
-                $value = $method ? $_GET[$field[0]] ?? '' : $_POST[$field[0]] ?? '';
-
-                $data[$field[0]] = $value ? self::sanitizeTypeInput($value, $field) : null;
-            }
+            $value = $method[$key] ?? null;
+            $data[$key] = $value ? self::sanitizeTypeInput($value, $expected, $key) : null;
         }
 
         return array_filter($data);
     }
 
-    protected static function sanitizeTypeInput(string $value, array $args = []): string
+    /**
+     * @param string $value
+     * @param $args
+     * @param string $key
+     * @return string
+     */
+    protected static function sanitizeTypeInput(string $value, $args, string $key = ''): string
     {
-        if ($value) {
-            if (in_array('email', $args)) {
-                $value = sanitize_email($value);
-            } elseif (in_array('password', $args)) {
-                return $value;
-            } else {
-                $value = sanitize_text_field($value);
-            }
+        if (strlen(trim($value)) <= 0) {
+            return '';
         }
 
-        return strlen($value) > 0 ? $value : '';
+        if (strpos($args, 'email') !== false || strpos($key, 'email') !== false) {
+            $value = sanitize_email($value);
+        } elseif (strpos($args, 'password') === false) {
+            $value = sanitize_text_field($value);
+        }
+
+        return $value;
     }
 }
--- src/Utils/Fields/Validate.php
+++ src/Utils/Fields/Validate.php
@@ -6,22 +6,81 @@ trait Validate
 {
     use Sanitize;
 
+    protected static array $counters = [
+        'array' => 'count',
+        'list' => 'count',
+        'lower' => 'strlen',
+        'string' => 'strlen',
+        'space' => 'strlen',
+        'upper' => 'strlen',
+        'xdigit' => 'strlen',
+    ];
+
+    protected static array $validators = [
+        'array' => 'is_array',
+        'bool' => 'is_bool',
+        'boolean' => 'is_bool',
+        'float' => 'is_float',
+        'int' => 'is_int',
+        'integer' => 'is_int',
+        'null' => 'is_null',
+        'object' => 'is_object',
+        'resource' => 'is_resource',
+        'scalar' => 'is_scalar',
+        'string' => 'is_string',
+    ];
+
+    protected static function checkExpecteds($value, string $expected, $field = ''): array
+    {
+        foreach (explode('|', $expected) as $item) {
+            [$type] = $item = explode(':', $item, 2);
+
+            $type = static::$validators[$type] ?? 'string';
+
+            if (!static::$validators[$type]($value)) {
+                self::setalerts($field, $item[0]);
+
+                continue;
+            }
+
+            if (isset($item[1])) {
+                $length = $value;
+
+                if (isset(static::$counters[$type])) {
+                    $length = static::$counters[$type]($value);
+                }
+
+                $range = explode(':', $item[1]);
+
+                if (($range[0] === '' || ($item[0] === 'max' ? $length >= $range[0] : $length < $range[0]))) {
+                    self::setalerts($field, $item[0]);
+                    continue;
+                }
+            }
+        }
+
+        self::$response['success'] = !(isset(self::$response['alerts']) && strpos($expected, 'required') !== false);
+
+        return self::$response;
+    }
+
     /**
      * @return array|bool
      */
-    protected static function getFieldsValidated()
+    protected static function getFieldsValidated($request = 'post')
     {
         $fields = static::getFields();
-        $values = self::sanitizeFields();
+        $values = self::sanitizeFields($request);
 
         if ($fields) {
-            foreach ($fields as $field) {
-                $field = is_array($field) ? $field : [];
+            foreach ($fields as $key => $requires) {
+                $field = is_string($requires) ? $requires : '';
+                $response = self::checkExpecteds($values[$key] ?? null, $field, $key);
 
-                if (in_array('required', $field)) {
-                    if (strlen(trim($values[$field[0]] ?? null)) <= 0) {
-                        return false;
-                    }
+                if (!$response['success']) {
+                    return $response;
+                } elseif (isset($response['alerts'])) {
+                    $values['alerts'][$key] = $response['alerts'] ?? null;
                 }
             }
 
@@ -29,5 +88,20 @@ trait Validate
         }
 
         return false;
+    }
+
+    /**
+     * @param $field
+     * @param $item
+     * @return void
+     */
+    protected static function setalerts($field, $item): void
+    {
+        var_dump($field);
+        var_dump($item);
+        self::$response['alerts'][] = [
+            'key' => $field,
+            'type' => $item,
+        ];
     }
 }
