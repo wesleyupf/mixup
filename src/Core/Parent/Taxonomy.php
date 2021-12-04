99853dfe60c4d587204fc3fe1e3c90eeea15cbfb
Agência UPFlex <agencia.upflex@gmail.com>
2021-12-04 15:19:04 -0300

    feat: implementando melhorias phpcs

--- src/Core/Parent/Taxonomy.php
+++ src/Core/Parent/Taxonomy.php
@@ -13,6 +13,52 @@ abstract class Taxonomy extends Base implements ITaxonomy
     public static string $tx_name = '';
     protected array $postTypes = [];
 
+    /**
+     * @param $instance
+     * @return array
+     */
+    public static function getLabels($instance): array
+    {
+        return [
+            "name" => $instance->getPlural(),
+            "singular_name" => $instance->getSingular(),
+            "menu_name" => $instance->getPlural(),
+            "all_items" => $instance->isMale()
+                ? sprintf(__("Todos os %s"), $instance->getPlural())
+                : sprintf(__("Todas as %s"), $instance->getPlural()),
+            "edit_item" => sprintf(__("Editar %s"), $instance->getSingular()),
+            "view_item" => sprintf(__("Ver %s"), $instance->getSingular()),
+            "update_item" => $instance->isMale()
+                ? sprintf(__("Atualizar nome do %s"), $instance->getSingular())
+                : sprintf(__("Atualizar nome da %s"), $instance->getSingular()),
+            "add_new_item" => $instance->isMale()
+                ? sprintf(__("Adicionar novo %s"), $instance->getSingular())
+                : sprintf(__("Adicionar nova %s"), $instance->getSingular()),
+            "new_item_name" => $instance->isMale()
+                ? sprintf(__("Novo %s"), $instance->getSingular())
+                : sprintf(__("Nova %s"), $instance->getSingular()),
+            "parent_item" => sprintf(__("%s ascendente"), $instance->getSingular()),
+            "parent_item_colon" => sprintf(__("%s ascendente:"), $instance->getSingular()),
+            "search_items" => sprintf(__("Pesquisar %s"), $instance->getPlural()),
+            "popular_items" => sprintf(__("%s mais populares"), $instance->getPlural()),
+            "separate_items_with_commas" => sprintf(__("Separe %s com vírgulas"), $instance->getPlural()),
+            "add_or_remove_items" => sprintf(__("Adicionar ou excluir %s"), $instance->getPlural()),
+            "choose_from_most_used" => sprintf(
+                __("Escolher entre os termos mais usados de %s"),
+                $instance->getPlural()
+            ),
+            "not_found" => $instance->isMale()
+                ? sprintf(__("Nenhum %s encontrado"), $instance->getSingular())
+                : sprintf(__("Nenhuma %s encontrada"), $instance->getSingular()),
+            "no_terms" => $instance->isMale()
+                ? sprintf(__("Nenhum %s"), $instance->getSingular())
+                : sprintf(__("Nenhuma %s"), $instance->getSingular()),
+            "items_list_navigation" => sprintf(__("Navegação na lista de %s"), $instance->getPlural()),
+            "items_list" => sprintf(__("Lista de %s"), $instance->getPlural()),
+            "back_to_items" => sprintf(__("Voltar para %s"), $instance->getPlural())
+        ];
+    }
+
     public function getPostTypes(): array
     {
         return $this->postTypes;
@@ -28,33 +74,9 @@ abstract class Taxonomy extends Base implements ITaxonomy
         $class = get_called_class();
         $instance = (new $class);
 
-        $labels = [
-            "name" => $instance->getPlural(),
-            "singular_name" => $instance->getSingular(),
-            "menu_name" => $instance->getPlural(),
-            "all_items" => $instance->isMale() ? sprintf(__("Todos os %s"), $instance->getPlural()) : sprintf(__("Todas as %s"), $instance->getPlural()),
-            "edit_item" => sprintf(__("Editar %s"), $instance->getSingular()),
-            "view_item" => sprintf(__("Ver %s"), $instance->getSingular()),
-            "update_item" => $instance->isMale() ? sprintf(__("Atualizar nome do %s"), $instance->getSingular()) : sprintf(__("Atualizar nome da %s"), $instance->getSingular()),
-            "add_new_item" => $instance->isMale() ? sprintf(__("Adicionar novo %s"), $instance->getSingular()) : sprintf(__("Adicionar nova %s"), $instance->getSingular()),
-            "new_item_name" => $instance->isMale() ? sprintf(__("Novo %s"), $instance->getSingular()) : sprintf(__("Nova %s"), $instance->getSingular()),
-            "parent_item" => sprintf(__("%s ascendente"), $instance->getSingular()),
-            "parent_item_colon" => sprintf(__("%s ascendente:"), $instance->getSingular()),
-            "search_items" => sprintf(__("Pesquisar %s"), $instance->getPlural()),
-            "popular_items" => sprintf(__("%s mais populares"), $instance->getPlural()),
-            "separate_items_with_commas" => sprintf(__("Separe %s com vírgulas"), $instance->getPlural()),
-            "add_or_remove_items" => sprintf(__("Adicionar ou excluir %s"), $instance->getPlural()),
-            "choose_from_most_used" => sprintf(__("Escolher entre os termos mais usados de %s"), $instance->getPlural()),
-            "not_found" => $instance->isMale() ? sprintf(__("Nenhum %s encontrado"), $instance->getSingular()) : sprintf(__("Nenhuma %s encontrada"), $instance->getSingular()),
-            "no_terms" => $instance->isMale() ? sprintf(__("Nenhum %s"), $instance->getSingular()) : sprintf(__("Nenhuma %s"), $instance->getSingular()),
-            "items_list_navigation" => sprintf(__("Navegação na lista de %s"), $instance->getPlural()),
-            "items_list" => sprintf(__("Lista de %s"), $instance->getPlural()),
-            "back_to_items" => sprintf(__("Voltar para %s"), $instance->getPlural())
-        ];
-
         $args = [
             "label" => $instance->getPlural(),
-            "labels" => $labels,
+            "labels" => self::getLabels($instance),
             "public" => true,
             "publicly_queryable" => true,
             "hierarchical" => false,
